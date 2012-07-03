18e49202289d3aea096b5a5ee582c92693a23f1a
kverschoor <kennyverschoor@msn.com>
2012-07-03 22:17:41 +0200

    Added Runnable thread to handle incoming connections

--- server/src/nl/wifidroid/main/WifiDroidServerActivity.java
+++ server/src/nl/wifidroid/main/WifiDroidServerActivity.java
@@ -1,5 +1,7 @@
 package nl.wifidroid.main;
 
+import java.net.ServerSocket;
+
 import nl.wifidroid.main.WifidroidService.LocalBinder;
 import android.app.Activity;
 import android.content.ComponentName;
@@ -8,30 +10,61 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
 import android.widget.Toast;
 
 public class WifiDroidServerActivity extends Activity {
 
-	private WifidroidService Backgroundservice;
-	private boolean isBound;
+	private WifidroidService Backgroundservice = null;
+
+	private Handler serviceHandler = new Handler(){
+		@Override
+		public void handleMessage(Message msg){
+			Toast.makeText(WifiDroidServerActivity.this, "Ontvangt iets",Toast.LENGTH_LONG).show();
+		}
+	};
 	
-    
+	private Runnable thread = new Thread(){
+		
+	@Override
+	public void run(){
+		
+		Looper.prepare();
+		Looper.loop();
+		
+		try{
+			
+			ServerSocket server = new ServerSocket(12345);
+
+			while((server.accept())!=null){
+			serviceHandler.handleMessage(null);
+			}
+			
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	};
+	
 	/** Called when the activity is first created. */
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       /* if(Backgroundservice == null){
+        
+        Toast.makeText(this, "Starting Intent", Toast.LENGTH_SHORT).show();
+        if(Backgroundservice == null)
+        {
         	doBindService();
-        	}
-        }*/
-        Toast.makeText(this, "Listening", Toast.LENGTH_LONG).show();
-
+        }
     }
-  
+
     private ServiceConnection service_connection = new ServiceConnection(){
     	public void onServiceConnected(ComponentName classname, IBinder service){
-    		Backgroundservice = ((LocalBinder)service).getService();
+    		Backgroundservice = ((LocalBinder) service).getService();
     		Toast.makeText(WifiDroidServerActivity.this, "Connected",Toast.LENGTH_LONG).show();
     	}
     	
